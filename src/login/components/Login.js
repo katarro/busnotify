531d8c097238a4e31b631a2543dd9210ad705486
felipe <pipe.castro.espinoza@gmail.com>
2023-04-18 00:38:57 -0400

    Sign in Google

--- src/login/components/Login.js
+++ src/login/components/Login.js
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./styles";
-import { Text, View, Image, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { useGoogleSignIn } from "../components/firebase/useGoogleSignIn";
+import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
+
+const logo = require("../../../assets/logoCompleto.png");
 
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const logo = require("../../../assets/logoCompleto.png");
-
-  const handleLogin = () => {
-    console.log(`Login con usuario: ${username} y contraseña: ${password}`);
-  };
+  const { token, userInfo, request, promptAsync } = useGoogleSignIn();
 
   return (
     <View style={styles.container}>
@@ -18,21 +15,17 @@ export default function Login() {
       </View>
       <View style={styles.formContainer}>
         <Text style={styles.label}>Email</Text>
-        <TextInput
-          value={username}
-          onChangeText={setUsername}
-          style={styles.input}
-        />
+        <TextInput value={null} onChangeText={null} style={styles.input} />
 
         <Text style={styles.label}>Password</Text>
         <TextInput
           secureTextEntry
-          value={password}
-          onChangeText={setPassword}
+          value={null}
+          onChangeText={null}
           style={styles.input}
         />
 
-        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <TouchableOpacity onPress={null} style={styles.button}>
           <Text style={styles.buttonText}>INGRESAR</Text>
         </TouchableOpacity>
 
@@ -40,7 +33,21 @@ export default function Login() {
 
         <View style={styles.socialButtonsContainer}>
           <TouchableOpacity style={styles.socialButton}>
-            <Text style={styles.buttonText}> Google</Text>
+            {/* Inicio Sesion con GOOGLE */}
+            {userInfo === null ? (
+              <Text
+                title="Google"
+                disabled={!request}
+                onPress={() => {
+                  promptAsync();
+                }}
+                style={styles.buttonText}
+              >
+                Google
+              </Text>
+            ) : (
+              <Text style={styles.text}>{userInfo.name}</Text>
+            )}
           </TouchableOpacity>
 
           <TouchableOpacity style={styles.FacebookButton}>
---
+++ src/login/components/firebase/useGoogleSignIn.js
@@ -0,0 +1,42 @@
+
+import { useState, useEffect } from "react";
+import * as Google from "expo-auth-session/providers/google";
+
+export const useGoogleSignIn = () => {
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "136480279207-mkua8bm84r32fjt4o37el64eu0vdh806.apps.googleusercontent.com",
+    iosClientId:
+      "136480279207-6li18p4h0b70euu6p0oc5e77jm8ofvbk.apps.googleusercontent.com",
+    webClientId:
+      "136480279207-qti1um1mmndjp8o34mttdl85b05ip7ol.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setToken(response.authentication.accessToken);
+      getUserInfo();
+    }
+  }, [response, token]);
+
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
+  return { token, userInfo, request, promptAsync };
+};
